6667621798952d08bb8ccd7d1f837cb456e656f3
AhzNimation <fatihahzaqusyairi24@gmail.com>
2022-01-29 14:06:16 +0800

    v 0.7

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
-function currentTime() {
-    var date = new Date(); /* creating object of Date class */
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    var midday = "AM";
-    midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
-    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
-      var t = setTimeout(currentTime, 1000); /* setting timer */
-  }
-  
-  function updateTime(k) { /* appending 0 before time elements if less than 10 */
-    if (k < 10) {
-      return "0" + k;
+const showJump = document.getElementById('showJump'),
+    jump = document.getElementById('jump');
+
+
+
+let jumpOpened = false;
+
+function check1() {
+    if (jumpOpened == false) {
+        showJump.innerHTML = '='
+        jump.classList.remove('slide')
+    } else if (jumpOpened == true) {
+        showJump.innerHTML = 'X'
+        jump.classList.add('slide')
+
     }
-    else {
-      return k;
+    setTimeout(check1);
+}
+
+function clickCheck() {
+    if (jumpOpened == false) {
+        jumpOpened = true;
+    } else if (jumpOpened == true) {
+        jumpOpened = false;
     }
-  }
-  
-  currentTime();
+}
+
+check1();
